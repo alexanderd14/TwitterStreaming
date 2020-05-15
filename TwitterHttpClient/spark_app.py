c908937c09969a50cf982dd82be182b75e761c84
alexanderd14 <56821696+alexanderd14@users.noreply.github.com>
2020-05-15 13:52:52 -0500

    Update spark_app.py

--- TwitterHttpClient/spark_app.py
+++ TwitterHttpClient/spark_app.py
@@ -35,7 +35,7 @@ def process_rdd(time, rdd):
         # Get spark sql singleton context from the current context
         sql_context = get_sql_context_instance(rdd.context)
         # convert the RDD to Row RDD
-        row_rdd = rdd.map(lambda w: Row(hashtag=w[0].encode("utf-8"), hashtag_count=w[1]))
+        row_rdd = rdd.map(lambda w: Row(hashtag=w[0], hashtag_count=w[1]))
         # create a DF from the Row RDD
         hashtags_df = sql_context.createDataFrame(row_rdd)
         # Register the dataframe as table
@@ -45,23 +45,28 @@ def process_rdd(time, rdd):
         hashtag_counts_df.show()
         # call this method to prepare top 10 hashtags DF and send them
         send_df_to_dashboard(hashtag_counts_df)
+        
     except:
         e = sys.exc_info()[0]
         print("Error: %s" % e)
 
+
+
 # split each tweet into words
 words = dataStream.flatMap(lambda line: line.split(" "))
+
 # filter the words to get only hashtags, then map each hashtag to be a pair of (hashtag,1)
-#hashtags = words.map(lambda x: (x, 1))
 hashtags = words.filter(lambda w: '#' in w).map(lambda x: (x, 1))
-# adding the count of each hashtag to its last count
-tags_totals = hashtags.updateStateByKey(aggregate_tags_count)
+
+# adding the count of each hashtag to its last count using updateStateByKey
+tags_totals = hashtags.reduceByKeyAndWindow(lambda x, y: int(x) + int(y), lambda x, y: int(x) - int(y), 600, 30)
+tags_totals.pprint()
+
 # do processing for each RDD generated in each interval
 tags_totals.foreachRDD(process_rdd)
 
 # start the streaming computation
 ssc.start()
+
 # wait for the streaming to finish
 ssc.awaitTermination()
-
-
